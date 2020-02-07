72de04351f9ac82adf987fadf51214bddc4e1321
earlMaxim <nechitaylo.maxim@gmail.com>
2020-02-07 13:13:55 +0200

    pictures making in DOM automaticaly

--- code.js
+++ code.js
@@ -20,19 +20,55 @@ btnTable.onclick = function(){
 
 }
 
-function makePictureRow(){
+function makeRowGallery(){
 
 }
-function makePictureTable(){
+
+makeTableGallery()
+
+function makeTableGallery(){
     //Gallery will be display as a tabble gallery
     let main = document.createElement("div");
     main.classList.add("main");
     gallery.append(main);
 
-    //Making each item of gallery
     let i=0, page=0;
+    //Making maximum 8 pictures on one page
     while (i<masterpieces.length || i<8){
+         //Making item of gallery
+        let item = document.createElement("div");
+        item.classList.add("item");
+        let img = document.createElement("div");
+        img.classList.add("img");
+        let text = document.createElement("div");
+        text.classList.add("text");
+        let pic = document.createElement("img")
+        pic.setAttribute("src", `img/${masterpieces[i].picture}`)
+        pic.setAttribute('alt',`${masterpieces[i].name}`)
+        img.append(pic);
+        let nameAndYear = document.createElement('div');
+        item.classList.add('nameAndYear')
+        let picName = document.createElement('h2');
+        picName.innerHTML = (`${masterpieces[i].name}`);
+        let picYear = document.createElement('h2');
+        picYear.innerHTML = (`${masterpieces[i].year}`)
+        nameAndYear.append(picName);
+        nameAndYear.append(picYear);
+        let autorAndStyle = document.createElement('div');
+        autorAndStyle.classList.add('autorAndStyle');
+        let picArtist = document.createElement('h3')
+        picArtist.innerHTML = (`${masterpieces[i].artist}`)
+        let picStyle = document.createElement('h4');
+        picStyle.innerHTML =(`${masterpieces[i].style}`)
+        autorAndStyle.append(picArtist);
+        autorAndStyle.append(picStyle);
 
+        text.append(nameAndYear);
+        text.append(autorAndStyle);
+        item.append(img);
+        item.append(text);
+        main.append(item)
+        i++
     }
 
 
