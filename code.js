456edbbc8a9aaa757f15f9b82956f0b6aba870fe
earlMaxim <nechitaylo.maxim@gmail.com>
2020-02-10 00:23:24 +0200

    All done

--- code.js
+++ code.js
@@ -5,7 +5,57 @@
 let masterpieces=[
     { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
     { picture:"Dance(II).jpg", name:"Танец (II)", year:"1928", artist:"Анри Матисс", style:"Экспрессионизм"},
-    { picture:"", name:"", year:"", artist:"", style:""}
+    { picture:"The_man_in_the_bowler_hat.jpg", name:"Человек в котелке", year:" 1964", artist:"Рене Магритт", style:"Сюрреализм"},
+    { picture:"Les_demoiselles_d'Avignon.jpg", name:"Авиньонские девицы", year:"1907", artist:"Пабло Пикассо", style:"Кубизм"},
+    { picture:"Small_worlds_4", name:"Маленькие миры 4", year:"1922", artist:"Василий Кандинский", style:"Абстракционизм"},
+    { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    // { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    // { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    // { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    // { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    // { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    // { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    // { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    // { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    // { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    // { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    // { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    // { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    // { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    // { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    // { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    // { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    // { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    // { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    // { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    // { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    // { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    // { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    // { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    // { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    // { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    // { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    // { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    // { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    // { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    // { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    // { picture:"The_Lovers.jpg", name:"Влюбленные", year:"1928", artist:"Рене Магритт", style:"Сюрреализм"},
+    // { picture:"", name:"", year:"", artist:"", style:""}
    
 ]
 
@@ -13,28 +63,115 @@ let btnRow =     document.querySelector(".row")
 let btnTable =   document.querySelector(".table")
 let gallery =    document.querySelector(".wrapper")
 
-btnRow.onclick = function(){
 
+console.log(gallery)
+
+let galleryRow = false;
+let galleryTable = false;
+
+btnRow.onclick = function(){
+    let firstpage = 1
+    makeRowGallery(firstpage)
 }
 btnTable.onclick = function(){
-
+    let firstpage = 1
+    makeTableGallery(firstpage)
 }
 
-function makeRowGallery(){
 
+function clearGallery(){
+    if(typeof(gallery.children[1]) == 'object'){
+        gallery.children[2].remove() //remove counter
+        gallery.children[1].remove() //remove gallery
+    }
 }
 
-makeTableGallery()
+function makeRowGallery(page) {
 
-function makeTableGallery(){
+    //firstly we have to clear previous gallery if we have it
+    clearGallery()
+
+    //Gallery will be displayed as a row gallery
+    let main = document.createElement("div");
+    main.classList.add("mainRow");
+    gallery.append(main);
+    //insert pages
+    makePageCounter();
+    let i=0;
+    //pagination
+    console.log("page is " + page)
+    let pageIs =     document.querySelector(".counter")
+    pageIs.onclick = function(event){
+        let page = event.target.innerHTML;
+        makeRowGallery(page);
+    }
+
+    let record = (page-1)*8
+    //Making maximum 8 pictures on one page
+    while(i<8){
+         //Making item of gallery
+        let item = document.createElement("div");
+        item.classList.add("itemRow");
+        let img = document.createElement("div");
+        img.classList.add("img");
+        let text = document.createElement("div");
+        text.classList.add("textRow");
+        let pic = document.createElement("img");
+        let test =masterpieces[record].picture
+        let picAdress = test
+        pic.setAttribute('src', `img/${picAdress}`);
+        pic.setAttribute('alt',`${masterpieces[record].name}`)
+        img.append(pic);
+        let nameAndYear = document.createElement('div');
+        item.classList.add('nameAndYear')
+        let picName = document.createElement('h2');
+        picName.innerHTML = (`${masterpieces[record].name}`);
+        let picYear = document.createElement('h2');
+        picYear.innerHTML = (`${masterpieces[record].year}`)
+        nameAndYear.append(picName);
+        nameAndYear.append(picYear);
+        let autorAndStyle = document.createElement('div');
+        autorAndStyle.classList.add('autorAndStyle');
+        let picArtist = document.createElement('h3')
+        picArtist.innerHTML = (`${masterpieces[record].artist}`)
+        let picStyle = document.createElement('h4');
+        picStyle.innerHTML =(`${masterpieces[record].style}`)
+        autorAndStyle.append(picArtist);
+        autorAndStyle.append(picStyle);
+
+        text.append(nameAndYear);
+        text.append(autorAndStyle);
+        item.append(img);
+        item.append(text);
+        main.append(item)
+        i++  
+        record++    
+    }
+}
+
+function makeTableGallery(page){
+
+    //firstly we have to clear previous gallery if we have it
+    clearGallery()
+
     //Gallery will be display as a tabble gallery
     let main = document.createElement("div");
     main.classList.add("main");
     gallery.append(main);
-
-    let i=0, page=0;
-    //Making maximum 8 pictures on one page
-    while (i<masterpieces.length || i<8){
+    //insert pages
+    makePageCounter();
+    let i=0;
+    //pagination
+    console.log("page is " + page)
+    let pageIs =     document.querySelector(".counter")
+    pageIs.onclick = function(event){
+        let page = event.target.innerHTML;
+        makeTableGallery(page);
+     }
+     // from which record we will show paintings -> masterpieces[record]
+     let record = (page-1)*8
+    //Making maximum 8 pictures in one page
+    while (i<8){
          //Making item of gallery
         let item = document.createElement("div");
         item.classList.add("item");
@@ -43,23 +180,23 @@ function makeTableGallery(){
         let text = document.createElement("div");
         text.classList.add("text");
         let pic = document.createElement("img")
-        pic.setAttribute("src", `img/${masterpieces[i].picture}`)
-        pic.setAttribute('alt',`${masterpieces[i].name}`)
+        pic.setAttribute('src', `img/${masterpieces[record].picture}`)
+        pic.setAttribute('alt',`${masterpieces[record].name}`)
         img.append(pic);
         let nameAndYear = document.createElement('div');
         item.classList.add('nameAndYear')
         let picName = document.createElement('h2');
-        picName.innerHTML = (`${masterpieces[i].name}`);
+        picName.innerHTML = (`${masterpieces[record].name}`);
         let picYear = document.createElement('h2');
-        picYear.innerHTML = (`${masterpieces[i].year}`)
+        picYear.innerHTML = (`${masterpieces[record].year}`)
         nameAndYear.append(picName);
         nameAndYear.append(picYear);
         let autorAndStyle = document.createElement('div');
         autorAndStyle.classList.add('autorAndStyle');
         let picArtist = document.createElement('h3')
-        picArtist.innerHTML = (`${masterpieces[i].artist}`)
+        picArtist.innerHTML = (`${masterpieces[record].artist}`)
         let picStyle = document.createElement('h4');
-        picStyle.innerHTML =(`${masterpieces[i].style}`)
+        picStyle.innerHTML =(`${masterpieces[record].style}`)
         autorAndStyle.append(picArtist);
         autorAndStyle.append(picStyle);
 
@@ -69,21 +206,20 @@ function makeTableGallery(){
         item.append(text);
         main.append(item)
         i++
+        record++  
     }
-
-
 }
 
-
-makePageCounter();
 function makePageCounter (){
     let counter = document.createElement("div");
     counter.classList.add("counter");
+
     gallery.append(counter);
 
     let i = 1 // i - number of page
     if(masterpieces.length > 8){ //each page has 8 pictures
-        while(masterpieces.length - 8 > 0){ //making pages
+       // Math.ceil(masterpieces.length/8)
+        for(;i<=Math.ceil(masterpieces.length/8);){ //making pages
             let page = document.createElement("div");
             page.classList.add("page");
             page.innerHTML= `${i}`
@@ -97,4 +233,4 @@ function makePageCounter (){
         page.innerHTML= `${i}`
         counter.append(page);
     }
-}
+ }
